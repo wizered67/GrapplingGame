0809adc7bc1403db782fe47c52053a94ab0d37c9
wizered67 <official.LKON@gmail.com>
2015-12-23 17:59:39 -0800

    Test

--- core/src/com/wizered67/game/GameScreen.java
+++ core/src/com/wizered67/game/GameScreen.java
@@ -18,7 +18,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 public class GameScreen implements Screen {
-	 
+	 //bleh test fml
 	//640 x 480
     MyGame game; // Note it's "MyGame" not "Game"
     SpriteBatch batch;
